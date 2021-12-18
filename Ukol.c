b4a83f0d9b2cd221a7d526299b44f6b27077c63f
PetaKasek <96347059+PetaKasek@users.noreply.github.com>
2021-12-18 17:43:59 +0100

    Create Ukol.c

---
+++ Ukol.c
@@ -0,0 +1,13 @@
+int main ()
+
+{
+   int h,m,s
+   int d=1000
+   printf("set time : \n");
+   scanf("%d%d%d",&h,&m,&s);
+   if(h>12  m>60  s>60
+   {
+     printf("ERROR ! \n");
+     exit(0)
+   } 
+}   
